fff5cf2dcc860fee991a8c4415e6e91c4f46dcc7
Yung Bill$ <billy.waite91@gmail.com>
2016-08-25 22:37:28 -0400

    added app.js

--- views/index.jsx
+++ views/index.jsx
@@ -5,7 +5,7 @@ export default class TodoBox extends React.Component {
         return (
             <div className="todoBox">
                 <h1>Todos</h1>
-                <TodoList />
+                <TodoList data={this.props.data}/>
                 <TodoForm />
             </div>
         );
@@ -14,15 +14,16 @@ export default class TodoBox extends React.Component {
 
 class TodoList extends React.Component {
     render() {
+        var todo = this.props.data.map(function (obj) {
+            return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>
+        });
         return (
             <div className="todoList">
-              <table style={{border: "2px solid black"}}>
-                <tbody>
-                  <Todo title="Shopping">Milk</Todo>
-                  <Todo title="Hair cut">13:00</Todo>
-                  <Todo title="Learn React">15:00</Todo>
-                </tbody>
-              </table>
+                <table style={{border: "2px solid black"}}>
+                    <tbody>
+                    {todo}
+                    </tbody>
+                </table>
             </div>
         );
     }
@@ -30,27 +31,41 @@ class TodoList extends React.Component {
 
 class Todo extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {checked: false};
+        super(props);
+        this.state = {
+            checked: false,
+            TodoStyle: style.notCheckedTodo
+        };
+        this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(e) {
-      this.setState({checked: e.target.checked});
+        this.setState({
+            checked: e.target.checked
+        });
+        if (e.target.checked) {
+            this.setState({
+                TodoStyle: style.checkedTodo
+            });
+        } else {
+            this.setState({
+                TodoStyle: style.notCheckedTodo
+            });
+        }
     }
 
     render() {
         return (
-            <tr>
-                <td style={{border: "1px solid black"}}>
-                  <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
+            <tr style={this.state.TodoStyle}>
+                <td style={style.tableContent}>
+                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
                 </td>
-                <td style={{border: "1px solid black"}}>{this.props.title}</td>
-                <td style={{border: "1px solid black"}}>{this.props.children}</td>
+                <td style={style.tableContent}>{this.props.title}</td>
+                <td style={style.tableContent}>{this.props.children}</td>
             </tr>
         );
     }
 }
-
 Todo.propTypes = {
     title: React.PropTypes.string.isRequired
 };
@@ -64,3 +79,15 @@ class TodoForm extends React.Component {
         );
     }
 }
+
+let style = {
+    checkedTodo: {
+        textDecoration: "line-through"
+    },
+    notCheckedTodo: {
+        textDecoration: "none"
+    },
+    tableContent: {
+        border: "1px solid black"
+    }
+};
